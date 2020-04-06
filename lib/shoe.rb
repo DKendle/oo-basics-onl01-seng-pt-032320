bf1459ca8f9b45cef2889fa6b85052fc689f3d57
Dina Kendle <dina.kendle@gmail.com>
2020-04-06 18:58:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,12 @@
 class Shoe 
-  attr_accessor :color :size :material:condition
-  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
-  attr_reader :brand
-
   
- 
-  
-  
-  
-  
-  
+  def cobble
+  end
   
 end
